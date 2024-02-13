647bb9ff5f87f482c380e2f3fa8b92812c5605c2
Cucumber05 <126766003+Cucumber05@users.noreply.github.com>
2024-02-14 00:17:40 +0300

    Add files via upload

--- VeddingMachine/VeddingMachine/VeddingMachine.cpp
+++ VeddingMachine/VeddingMachine/VeddingMachine.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <string>
 void ShowSelection();
-void SellProduct();
+void SellProduct(dispenserType& product);
 
 dispenserType Chips(37, 85);
 dispenserType Peanut(50, 40);
@@ -12,11 +12,31 @@ cashRegister CashRegister(0);
 
 int main() {
 	ShowSelection();
+	int item;
 	std::cout << "do u want some? (yes/no)" << std::endl;
 	std::string contin;
 	std::cin >> contin;
 	while (contin != "no") {
-		SellProduct();
+
+		std::cout << "<3 enter wich one(1-4):" << std::endl;
+		std::cin >> item;
+		switch (item) {
+		case 1:
+			SellProduct(Chips);
+			break;
+		case 2:
+			SellProduct(Peanut);
+			break;
+		case 3:
+			SellProduct(Chocolate);
+			break;
+		case 4:
+			SellProduct(Sandwich);
+			break;
+		default:
+			std::cout << "Please try another one :(" << std::endl;
+			break;
+		}
 		std::cout << "do u want some more? (yes/no)" << std::endl;
 		std::cin >> contin;
 
@@ -34,87 +54,24 @@ void ShowSelection() {
 	std::cout << "4.Sandwich			  	 " << Sandwich.getCost() << "	  " << Sandwich.getNoOfItems() << std::endl;
 } 
 
-void SellProduct() {
-	int item;
+void SellProduct(dispenserType& product) {
 	int cash;
-	std::cout << "<3 enter wich one(1-4):" << std::endl;
-	std::cin >> item;
-	switch (item) {
-	case 1:
-		if (Chips.getNoOfItems() > 0 ) {
-			std::cout << "It cost: " << Chips.getCost() << " rubles" << std::endl;
 
-			while (CashRegister.getCurrentBalance() < Chips.getCost())
+		if (product.getNoOfItems() > 0 ) {
+			std::cout << "It cost: " << product.getCost() << " rubles" << std::endl;
+
+			while (CashRegister.getCurrentBalance() < product.getCost())
 			{
-				std::cout << "Give me ur money :D  add " << Chips.getCost() - CashRegister.getCurrentBalance() << " rubles more" << std::endl;
+				std::cout << "Give me ur money :D  add " << product.getCost() - CashRegister.getCurrentBalance() << " rubles more" << std::endl;
 				std::cin >> cash;
 				CashRegister.acceptAmount(cash);
 			}
-			Chips.makeSale();
-			CashRegister.acceptAmount(Chips.getCost() * (-1));
-			std::cout << "Tnx! Dont forget take ur chips! Yammyy <333" << std::endl;
+			product.makeSale();
+			CashRegister.acceptAmount(product.getCost() * (-1));
+			std::cout << "Tnx! Dont forget take ur food! Yammyy <333" << std::endl;
 			std::cout << "Ur balance:" << CashRegister.getCurrentBalance() << " rubles" << std::endl;
 
 		}
-		else { std::cout << "Sorry >'_'< We're out of chips :(" << std::endl; }
-		break;
+		else { std::cout << "Sorry >'_'< We're out of that :(" << std::endl; }
 
-	case 2:
-		if (Peanut.getNoOfItems() > 0) {
-			std::cout << "It cost: " << Peanut.getCost() << " rubles" << std::endl;
-
-			while (CashRegister.getCurrentBalance() < Peanut.getCost())
-			{
-				std::cout << "Give me ur money :D add " << Peanut.getCost() - CashRegister.getCurrentBalance() << " rubles more" << std::endl;
-				std::cin >> cash;
-				CashRegister.acceptAmount(cash);
-			}
-			Peanut.makeSale();
-			CashRegister.acceptAmount(Peanut.getCost() * (-1));
-			std::cout << "Tnx! Dont forget take ur peanut! Yammyy <333" << std::endl;
-			std::cout << "Ur balance: " << CashRegister.getCurrentBalance() << " rubles" << std::endl;
-		}
-		else { std::cout << "Sorry >'_'< We're out of peanut :(" << std::endl; }
-		break;
-
-	case 3:
-		if (Chocolate.getNoOfItems() > 0) {
-			std::cout << "It cost: " << Chocolate.getCost() << " rubles" << std::endl;
-
-			while (CashRegister.getCurrentBalance() < Chocolate.getCost())
-			{
-				std::cout << "Give me ur money :D add " << Chocolate.getCost() - CashRegister.getCurrentBalance() << " rubles more" << std::endl;
-				std::cin >> cash;
-				CashRegister.acceptAmount(cash);
-			}
-			Chocolate.makeSale();
-			CashRegister.acceptAmount(Chocolate.getCost() * (-1));
-			std::cout << "Tnx! Dont forget take ur chocolate! Yammyy <333" << std::endl;
-			std::cout << "Ur balance: " << CashRegister.getCurrentBalance() << "rubles" << std::endl;
-		}
-		else { std::cout << "Sorry >'_'< We're out of chocolate :(" << std::endl; }
-		break;
-
-	case 4:
-		if (Sandwich.getNoOfItems() > 0) {
-			std::cout << "It cost: " << Sandwich.getCost() << " rubles" << std::endl;
-
-			while (CashRegister.getCurrentBalance() < Sandwich.getCost())
-			{
-				std::cout << "Give me ur money :D add " << Sandwich.getCost() - CashRegister.getCurrentBalance() << " rubles more" << std::endl;
-				std::cin >> cash;
-				CashRegister.acceptAmount(cash);
-			}
-			Sandwich.makeSale();
-			CashRegister.acceptAmount(Sandwich.getCost() * (-1));
-			std::cout << "Tnx! Dont forget take ur sandwich! Yammyy <333" << std::endl;
-			std::cout << "Ur balance: " << CashRegister.getCurrentBalance() << " rubles" << std::endl;
-		}
-		else { std::cout << "Sorry >'_'< We're out of sandwich :(" << std::endl; }
-		break;
-	
-    default:
-        std::cout << "Please try another one :(" << std::endl;
-        break;
-    }
 }
