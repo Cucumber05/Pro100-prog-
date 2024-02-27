a7f338d04b80089a13f58fe2264738cbd9cd89b8
Cucumber05 <126766003+Cucumber05@users.noreply.github.com>
2024-02-27 21:36:00 +0300

    Add files via upload

---
+++ Matrix lab.cpp
@@ -0,0 +1,10 @@
+﻿// Matrix.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include "Matrix.h"
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
---
+++ Matrix.h
@@ -0,0 +1,12 @@
+#pragma once
+class Matrix_Class
+{
+private:
+	int** data;
+	unsigned int m, n;
+public:
+	Matrix_Class() {
+
+	}
+};
+
