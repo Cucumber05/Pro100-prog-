afc91f7eb39f9d077fca2316693e953e5abb5ccd
Cucumber05 <126766003+Cucumber05@users.noreply.github.com>
2024-03-06 23:59:40 +0300

    Add files via upload

--- Matrix.h
+++ Matrix.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <iostream>
+#include <string>
+#include <cstdlib>
 
 template<typename T>
 class Matrix_Class
@@ -8,6 +10,7 @@ private:
 	T* data;
 	int m, n;
 public:
+
 	Matrix_Class(int _m, int _n) {  //прямоугольная матрица
 		m = _m;
 		n = _n;
@@ -37,6 +40,69 @@ public:
 	~Matrix_Class(){
 		delete[] data;
 	}
+
+	Matrix_Class& operator= (const Matrix_Class& matrix) {
+		this-> ~Matrix_Class(); //
+
+		n = matrix.n;
+		m = matrix.m;
+
+		data = new T[m * n];
+		for (int i = 0; i < m * n; i++) {
+			data[i] = matrix.data[i];
+		}
+		return *this;
+	}
+
+
+	bool operator!= (const Matrix_Class& matrix) {
+		return 1 - (*this == matrix);
+
+	}
+	Matrix_Class operator+ (const Matrix_Class& matrix) {
+		if (m == matrix.m && n == matrix.n) {
+			Matrix_Class result(m, n);
+
+			for (int i = 0; i < m * n; i++) {
+				
+				result.data[i] = data[i] + matrix.data[i];
+			}
+			return result;
+		}
+
+
+	}
+
+	Matrix_Class operator- (const Matrix_Class& matrix) {
+		if (m == matrix.m && n == matrix.n) {
+			Matrix_Class result(m, n);
+
+			for (int i = 0; i < m * n; i++) {
+
+				result.data[i] = data[i] - matrix.data[i];
+			}
+			return result;
+		}
+
+	}
+
+	Matrix_Class operator* (const Matrix_Class& matrix) {
+		if (n== matrix.m) {
+			Matrix_Class result(m, matrix.n);
+
+			for (int i = 0; i < m; i++) {
+				for (int j = 0; j < matrix.n; j++) {
+
+					result.data[i * result.n + j ] = 0;
+					for (int k = 0; k < n; k++)
+						result.data[i * result.n + j] += data[i * n + k] * matrix.data[k * matrix.n + j];
+				}
+
+			}
+			return result;
+		}
+	}
+
 	template<typename U>
 	friend std::istream& operator>> (std::istream& s, Matrix_Class<U>& matrix);
 
